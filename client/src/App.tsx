0e22703a05ab2692d786d61264db64a5b05e8f49
abi-shek-dev <abishekinterspace@gmail.com>
2026-05-20 00:47:54 +0530

    Route defining

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,48 @@
+import { Toaster } from 'react-hot-toast'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import AppLayout from './pages/AppLayout'
+import Products from './pages/Products'
+import ProductPage from './pages/ProductPage'
+import Home from './pages/Home'
+import SearchResult from './pages/SearchResult'
+import FlashDeals from './pages/FlashDeals'
+import Checkout from './pages/Checkout'
+import MyOrders from './pages/MyOrders'
+import OrderTracking from './pages/OrderTracking'
+import Addresses from './pages/Addresses'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Toaster position='top-right' toastOptions={{ duration: 3000, style: { background: '#1B3022', color: '#fff', borderRadius: '12px', fontSize: '14px' } }} />
+
+      <Routes>
+
+        {/* Auth pages - No Navbar/Footer */}
+
+        <Route path='/login' element={<Login />} />
+
+        {/* Main pages - With Navbar/Footer */}
+        <Route path='/' element={<AppLayout />} >
+          <Route index element={<Home />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:id' element={<ProductPage />} />
+          <Route path='search' element={<SearchResult />} />
+          <Route path='deals' element={<FlashDeals />} />
+          <Route >
+            <Route path='checkout' element={<Checkout />} />
+            <Route path='orders' element={<MyOrders />} />
+            <Route path='orders/:id' element={<OrderTracking />} />
+            <Route path='addresses' element={<Addresses />} />
+          </Route>
+        </Route>
+
+      </Routes>
+
+
+    </>
   )
 }
 
